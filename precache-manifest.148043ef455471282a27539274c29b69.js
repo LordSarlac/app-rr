6ad29cf93a48e38d9fe2bbf0563c5288d5538df5
Wojtas <Wojtas@wp.plgit config --global user.email Wojtas@wp.plgit config --global user.email Wojtas@wp.pl>
2020-07-24 13:56:14 +0200

    Updates

---
+++ precache-manifest.148043ef455471282a27539274c29b69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e99aafc9c01afa9c745",
+    "url": "/app-rr/./static/css/main.f389a49d.chunk.css"
+  },
+  {
+    "revision": "4e99aafc9c01afa9c745",
+    "url": "/app-rr/./static/js/main.4e99aafc.chunk.js"
+  },
+  {
+    "revision": "164ff452ad7acad99b13",
+    "url": "/app-rr/./static/js/1.164ff452.chunk.js"
+  },
+  {
+    "revision": "087d44ddcb8154a7e9a6",
+    "url": "/app-rr/./static/js/runtime~main.087d44dd.js"
+  },
+  {
+    "revision": "e0a6bb2a8fc0a0cb3cbc7c26461f5afa",
+    "url": "/app-rr/./index.html"
+  }
+];
